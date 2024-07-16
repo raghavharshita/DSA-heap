5cfcdf0f1a4fa2ccaeba4748d1896d8f0a0baded
Harshita Singh Raghav <raghavharshita515@gmail.com>
2024-07-16 11:22:36 +0530

    max_heap

---
+++ insertion_of_heap_using_array.cpp
@@ -0,0 +1,42 @@
+// implementation of max heap
+#include<bits/stdc++.h>
+using namespace std;
+class heap{
+    public:
+    int arr[100];
+    int size=0;
+
+    void insert(int val){
+        size++;
+        arr[size]=val;
+        int index=size;
+        while(index>1){
+            int parentidx=index/2;
+            if(arr[parentidx]>arr[index]){
+                return;
+            }
+            else{
+                swap(arr[parentidx],arr[index]);
+                index=parentidx;
+            }
+        }
+    }
+    
+    void print(){
+        for(int i=1;i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main(){
+    heap h;
+    h.insert(50);
+    h.insert(85);
+    h.insert(96);
+    h.insert(23);
+    h.insert(14);
+    h.insert(49);
+    h.print();
+}
